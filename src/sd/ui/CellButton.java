d086b1b8a2fce0596b22bbd8c9f4459789914274
lucapompei <lucapompei@mbp-di-luca.lan>
2015-01-23 10:19:30 +0100

    Cell listener

--- src/sd/ui/CellButton.java
+++ src/sd/ui/CellButton.java
@@ -1,21 +1,39 @@
 package sd.ui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+import sd.core.Cell;
 
 public class CellButton extends JButton {
 	
 	private static final long serialVersionUID = 1L;
-	private int row, col;
+	private int row;
+	private int col;
+	private int rowOnGameBoard;
+	private int colOnGameBoard;
 		
-	public CellButton(int pRow, int pCol, String path) {
+	public CellButton(int pRow, int pCol, String path, Cell cell) {
 		super("");
 		this.row = pRow;
 		this.col = pCol;
+		this.rowOnGameBoard = cell.getRow();
+		this.colOnGameBoard = cell.getColumn();
 		this.setIcon(new javax.swing.ImageIcon(getClass().getResource(path)));
 		this.setBorder(null);
 		this.setFocusPainted(false);
 		this.setBorderPainted(false);
 		this.setContentAreaFilled(false);
+		this.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Hai cliccato:\nla cella in matrice ("+rowOnGameBoard+", "+colOnGameBoard+")'\nla cella nella gui ("+row+", "+col+")");
+			}
+		});
+		
 	}
 
 	public int getRow() {
@@ -24,6 +42,22 @@ public class CellButton extends JButton {
 
 	public int getCol() {
 		return col;
+	}
+
+	public int getRowOnGameBoard() {
+		return rowOnGameBoard;
+	}
+
+	public void setRowOnGameBoard(int rowOnGameBoard) {
+		this.rowOnGameBoard = rowOnGameBoard;
+	}
+
+	public int getColOnGameBoard() {
+		return colOnGameBoard;
+	}
+
+	public void setColOnGameBoard(int colOnGameBoard) {
+		this.colOnGameBoard = colOnGameBoard;
 	}
 
 }
