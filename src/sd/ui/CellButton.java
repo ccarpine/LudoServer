17b5b8ca13ed8ec7cef7478ae17cbd0b7eed338e
lucapompei <lucapompei@mbp-di-luca.lan>
2015-01-27 15:51:50 +0100

    up

--- src/sd/ui/CellButton.java
+++ src/sd/ui/CellButton.java
@@ -80,7 +80,7 @@ public class CellButton extends JButton implements Runnable {
 			public void actionPerformed(ActionEvent e) {
 				if (cell.getPawns().size() == 1) {
 					setIcon(new javax.swing.ImageIcon(
-									ClassLoader.getSystemResource("sd/ui/images/box/"+folder+"/"+cell.getColor()+"_"+cell.getColor()+".png")));
+									ClassLoader.getSystemResource("sd/ui/images/box/"+folder+"/"+cell.getColor()+"_"+cell.getPawns().get(0)+".png")));
 				} else {
 					setIcon(new javax.swing.ImageIcon(
 									ClassLoader.getSystemResource("sd/ui/"+basePath)));
