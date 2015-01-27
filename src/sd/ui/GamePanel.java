ed3b0bc019b84b6f26fb48152073c8c14b30811e
Claudia <Claudia@claudia.lan>
2015-01-27 15:41:34 +0100

    1qq

--- src/sd/ui/GamePanel.java
+++ src/sd/ui/GamePanel.java
@@ -309,10 +309,12 @@ public class GamePanel extends BGPanel {
 			return new Cell(Constants.COLOR[colorBenchIndex], colorBenchIndex, Constants.COLUMNS-1+this.coreGame.getMyPartecipant().getPawnsInBench());
 		// find pawn position in the whole game board
 		} else {
-			System.out.println("colonna   "+ cellPosition.getColumn());
 			System.out.println("riga   "+ cellPosition.getRow());
+			System.out.println("colonna   "+ cellPosition.getColumn());
+			
 			for (int i=0; i<Constants.ROWS; i++) {
 				for (int j=0; j<Constants.COLUMNS+Constants.BENCH_DIMENSION; j++) {
+					System.out.println(this.cellsButton[i][j].getCell().getRow()+" "+this.cellsButton[i][j].getCell().getColumn());
 					if (this.cellsButton[i][j].getCell().getRow() == cellPosition.getRow() &&
 							this.cellsButton[i][j].getCell().getColumn() == cellPosition.getColumn()) {
 						return new Cell(Constants.COLOR[i], i, j);
