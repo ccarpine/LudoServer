54260c6865d75c3e306c2ffa2c7e04915cd60c1e
Claudia <Claudia@claudia.lan>
2015-01-27 17:56:17 +0100

    qq

--- src/sd/core/Cell.java
+++ src/sd/core/Cell.java
@@ -74,8 +74,16 @@ public class Cell implements Serializable{
 				return Constants.FAIL;
 		// rules if it is your goal
 		else if (this.color.equals(colorPawn)) {
-			if (this.pawns.size() == 0 || (this.pawns.size() < 2 && this.column == 0))
+			if (this.pawns.size() == 0 )
 				return Constants.SUCCESS;
+			else if (this.pawns.size() == 1 && this.column == 0) {
+				if (this.pawns.get(0).equals(colorPawn)){
+					return Constants.SUCCESS;
+				}
+				else {
+					return Constants.EATEN;
+				}
+			}
 			else
 				return Constants.FAIL;
 		}
