9491ccd80adaef8bdc356386a45e1cc6fd6e9b7c
lucapompei <lucapompei@mbp-di-luca.lan>
2015-01-28 18:18:41 +0100

    das

--- src/sd/ui/GamePanel.java
+++ src/sd/ui/GamePanel.java
@@ -31,7 +31,7 @@ public class GamePanel extends BGPanel {
 	 * @param coreGame
 	 */
 	public GamePanel(CoreGame coreGame, UserPlayer userPlayer) {
-		super("images/pattern.png");
+		super("images/carpet2.jpg");
 		this.setPreferredSize(new java.awt.Dimension(570, 532));
 		this.coreGame = coreGame;
 		this.userPlayer = userPlayer;
