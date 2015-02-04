a9f865d29dac7d91e56499514ff45ae552ded709
lucapompei <lucapompei@mbp-di-luca.lan>
2015-02-04 17:58:38 +0100

    q

--- src/sd/util/MyFont.java
+++ src/sd/util/MyFont.java
@@ -14,15 +14,14 @@ public class MyFont {
 		try {
 			Font font = Font.createFont(Font.TRUETYPE_FONT, 
 					ClassLoader.getSystemResource("sd/ui/font/funSized.ttf").openStream());
-			System.out.println("Si font");
-	        font = font.deriveFont(Font.BOLD, size);
+			font = font.deriveFont(Font.BOLD, size);
 	        GraphicsEnvironment ge =
 	            GraphicsEnvironment.getLocalGraphicsEnvironment();
 	        ge.registerFont(font);
 	        return font;
 		} catch (FontFormatException | IOException e) {
 			e.printStackTrace();
-			return null;
+			return Font.getFont("Helvetica");
 		}
 	}
 
