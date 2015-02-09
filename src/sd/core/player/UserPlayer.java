1a0719e5698416ba373194cf9332bf3ff5b582fc
Claudia <Claudia@claudia>
2015-02-09 12:08:53 +0100

    11

--- src/sd/core/player/UserPlayer.java
+++ src/sd/core/player/UserPlayer.java
@@ -57,7 +57,7 @@ public class UserPlayer extends UnicastRemoteObject implements
 		this.mainFrame.addPanel(new IntroPanel(), BorderLayout.CENTER);
 		this.whenCrash = 0;
 		this.turnCrash = 0;
-		this.whoCrash = 0;
+		this.whoCrash = 1;
 	}
 
 	/**
