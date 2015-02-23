91e39423f1cc752aac866a8d69d35f0b26d74c68
Lory <silent.lory@gmail.com>
2015-02-23 16:54:11 +0100

    ...

--- src/sd/core/Move.java
+++ src/sd/core/Move.java
@@ -10,8 +10,8 @@ public class Move implements Serializable{
 	
 	/** 
 	 * It creates a move for a pawn from a cell (which might be null if a new pawn is going to be inserted) to another
-	 * @param Cell, the cell from which the move will be performed
-	 * @param Cell, the cell into which the move will get
+	 * @param start Cell from which the move will be performed
+	 * @param destination Cell into which the move will get
 	 */
 	public Move(Cell start, Cell destination) {
 		this.start = start;
